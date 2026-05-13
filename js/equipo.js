ec1448cc77f7d3848dd3ef597ca587fce5ce1a99
Vtomii <tomas_2001_medanos@hotmail.com>
2026-05-13 19:58:36 -0300

    creo el archivo equipo.js para comunicar los datos del back con el front

---
+++ js/equipo.js
@@ -0,0 +1,40 @@
+async function cargarEquipo() {
+
+    try {
+
+        const response = await fetch('http://localhost:3000/equipo');
+
+        const equipo = await response.json();
+
+        const contenedor = document.getElementById('contenedorEquipo');
+
+        equipo.forEach(persona => {
+
+            contenedor.innerHTML += `
+            
+                <div class="Presentacion">
+
+                    <h2>${persona.nombre}</h2>
+
+                    <img 
+                        class="Imagen_equipo"
+                        src="${persona.imagen}"
+                        alt="${persona.nombre}"
+                    >
+
+                    <h3>${persona.cargo}</h3>
+
+                    <p>${persona.descripcion}</p>
+
+                </div>
+            `;
+        });
+
+    } catch (error) {
+
+        console.log('Error:', error);
+
+    }
+}
+
+cargarEquipo();
